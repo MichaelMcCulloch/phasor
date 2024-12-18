e1eef460a9f99a91223d499756d3970696bb0af6
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2024-12-17 18:39:59 -0700

    repackage candle

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,5 @@ mod ops;
 pub use operands::*;
 pub use ops::*;
 
-pub use candle_core::{DType, Device};
-pub use num_complex::{c32, c64, Complex, Complex32, Complex64, ComplexFloat, ParseComplexError};
+pub use candle_core::*;
+pub use num_complex::*;

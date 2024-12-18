cd68f88ec5c350fe63243a87e802fea005b1a2e3
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2024-12-17 18:32:30 -0700

    forward library elements

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,6 @@ mod ops;
 
 pub use operands::*;
 pub use ops::*;
+
+pub use candle_core::{DType, Device};
+pub use num_complex::{c32, c64, Complex, Complex32, Complex64, ComplexFloat, ParseComplexError};
